fa6bb05577539d4119a52405d004ca1c3fd62069
dzid26 <dzidmail@gmail.com>
2023-07-17 23:03:55 +0100

    Blinky testing with semihosting

--- unit-testing/semihosting/src/main.c
+++ unit-testing/semihosting/src/main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "main.h"
 #include "stm32l4xx_hal.h"
 
 extern void initialise_monitor_handles(void);
 
+void LED_Init()
+{
+  LED_GPIO_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+
+}
 int main(int argc, char* argv[]) {
     
     initialise_monitor_handles();
 
     HAL_Init();
+    LED_Init();
 
     while(1) {
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
         printf("Hello world!\n");
         HAL_Delay(1000);
     }
--- unit-testing/semihosting/test/test_dummy/test_main.c
+++ unit-testing/semihosting/test/test_dummy/test_main.c
@@ -17,26 +17,57 @@
 #include "stm32l4xx_hal.h"
 #include <unity.h>
 
-void setUp(void) {}
-void tearDown(void) {}
+#include "main.h"
 
-void test_dummy(void)
-{
-    TEST_ASSERT_EQUAL(1, 1);
+void setUp(void) {
+    LED_GPIO_CLK_ENABLE();
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = LED_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 }
 
-int main()
-{
+void tearDown(void) {
+    HAL_GPIO_DeInit(LED_GPIO_PORT, LED_PIN);
+}
+
+void test_led_builtin_pin_number(void) {
+    TEST_ASSERT_EQUAL(LED_PIN, GPIO_PIN_5);
+}
+
+void test_led_state_high(void) {
+    HAL_GPIO_WritePin(LED_GPIO_PORT, LED_PIN, GPIO_PIN_SET);
+    TEST_ASSERT_EQUAL(HAL_GPIO_ReadPin(LED_GPIO_PORT, LED_PIN), GPIO_PIN_SET);
+}
+
+void test_led_state_low(void) {
+    HAL_GPIO_WritePin(LED_GPIO_PORT, LED_PIN, GPIO_PIN_RESET);
+    TEST_ASSERT_EQUAL(HAL_GPIO_ReadPin(LED_GPIO_PORT, LED_PIN), GPIO_PIN_RESET);
+}
+
+int main() {
+
+    HAL_Init();
+    HAL_Delay(2000);
+    
     UNITY_BEGIN();
-    RUN_TEST(test_dummy);
+    RUN_TEST(test_led_builtin_pin_number);
+    for (unsigned int i = 0; i < 5; i++)
+    {
+        RUN_TEST(test_led_state_high);
+        HAL_Delay(500);
+        RUN_TEST(test_led_state_low);
+        HAL_Delay(500);
+    }
+
     UNITY_END(); // stop unit testing
 
-    while (1)
-    {
-    }
+    while(1){}
 }
 
-void SysTick_Handler(void)
-{
-    HAL_IncTick();
+void SysTick_Handler(void) {
+  HAL_IncTick();
 }
